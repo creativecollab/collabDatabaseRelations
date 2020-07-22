d2d3c26d05bc88f01775b5cbc98dec39de260658
m90k <m90kdeveloper@gmail.com>
2020-07-22 20:26:07 -0300

    Feature Create Backedn

--- src/modules/orders/services/FindOrderService.ts
+++ src/modules/orders/services/FindOrderService.ts
@@ -1,7 +1,5 @@
 import { inject, injectable } from 'tsyringe';
 
-import IProductsRepository from '@modules/products/repositories/IProductsRepository';
-import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -11,14 +9,16 @@ interface IRequest {
 
 @injectable()
 class FindOrderService {
-  constructor(
-    private ordersRepository: IOrdersRepository,
-    private productsRepository: IProductsRepository,
-    private customersRepository: ICustomersRepository,
-  ) {}
+  constructor(@inject('OrdersRepository')
+    private ordersRepository: IOrdersRepository) {
+
+    }
 
   public async execute({ id }: IRequest): Promise<Order | undefined> {
     // TODO
+    const order = await this.ordersRepository.findById(id);
+
+    return order;
   }
 }
 
